d8d3bd969ddbae5f75f5d07e68668eb64def389a
sxpientdream <118003169+sxpientdream@users.noreply.github.com>
2024-06-04 23:39:27 +0600

    Update gamestat.js

--- js/gamestat.js
+++ js/gamestat.js
@@ -1,9 +1,14 @@
-document.getElementById("openTabLink").addEventListener("click", function(event) {
-    event.preventDefault();
-    var gameStats = document.getElementById("gameStats");
-    if (gameStats.classList.contains("visible")) {
-        gameStats.classList.remove("visible");
-    } else {
-        gameStats.classList.add("visible");
-    }
+document.querySelectorAll(".openTabLink").forEach(function(button) {
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
+        var tabId = button.getAttribute("data-tab");
+        var gameStats = document.getElementById(tabId);
+        if (gameStats.classList.contains("visible")) {
+            gameStats.classList.remove("visible");
+            gameStats.classList.add("hidden");
+        } else {
+            gameStats.classList.remove("hidden");
+            gameStats.classList.add("visible");
+        }
+    });
 });
