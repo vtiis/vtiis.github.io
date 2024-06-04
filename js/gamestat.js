0f03fe8f36ec6d3303b14ceeacf96026244130a5
sxpientdream <118003169+sxpientdream@users.noreply.github.com>
2024-06-05 00:36:55 +0600

    вфывфыв

--- js/gamestat.js
+++ js/gamestat.js
@@ -1,14 +1,14 @@
-document.querySelectorAll('.openTabLink').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const tabId = this.getAttribute('data-tab');
-        const gameStats = document.getElementById(tabId);
-        if (gameStats.classList.contains('visible')) {
-            gameStats.classList.remove('visible');
-            gameStats.classList.add('hidden');
-        } else {
-            gameStats.classList.remove('hidden');
-            gameStats.classList.add('visible');
-        }
-    });
-});
+        document.querySelectorAll('.openTabLink').forEach(link => {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                const tabId = this.getAttribute('data-tab');
+                const table = document.getElementById(tabId);
+                if (table.classList.contains('hidden')) {
+                    table.classList.remove('hidden');
+                    table.classList.add('visible');
+                } else {
+                    table.classList.remove('visible');
+                    table.classList.add('hidden');
+                }
+            });
+        });
